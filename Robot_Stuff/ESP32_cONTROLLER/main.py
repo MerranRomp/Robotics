5dd6ede9b69e24fc1afaa3bddab1099a5f95fc4b
MerranRomp <merran@evabits.com>
2025-06-11 21:30:12 +0200

    made changes to start testing sensor readings
    Not yet working but progress

---
+++ Robot_Stuff/ESP32_cONTROLLER/main.py
@@ -0,0 +1,143 @@
+from machine import Pin, I2C, UART
+from time import ticks_ms, ticks_diff, sleep, time, ticks_us
+from VL53L0X import VL53L0X
+from Utils import SeeFunctions, ThinkFunctions, ActFunctions
+import math
+
+pin_a = Pin(18, Pin.IN)
+pin_b = Pin(19, Pin.IN)
+
+
+
+#variables
+tick_count = 0
+last_a = 0
+counter = 0
+COUNTER_MAX = 5
+COUNTER_STOP = 50
+PPR = 11  # encoder pulses per motor revolution (check your encoder datasheet)
+GEAR_RATIO = 30  # gearbox reduction ratio
+WHEEL_DIAMETER_CM = 6.5
+recognition_distance = 200 # Distance in mm to recognize an object
+
+
+
+# Set serial to UART1 using the same pins as UART0 to communicate via USB
+uart = UART(1, 115200, tx=1, rx=3)
+
+#Setup Sensors
+SeeFunctions.setup_ir_sensors(32, 33, 34, 35, 25) # Pins for IR sensors (adjust as needed)
+tof = SeeFunctions.setup_VL53L0X()
+
+pin_a.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=SeeFunctions.update)
+
+
+
+# Setup buttons
+button_left = Pin(34, Pin.IN, Pin.PULL_DOWN)
+button_right = Pin(35, Pin.IN, Pin.PULL_DOWN)
+
+# Wait for the button click before changing the serial port to UART1.
+# During the wait period, the program can be stopped using the STOP button.
+print("Click the button on the ESP32 to continue. Then, close Thonny and run the Webots simulation.")
+print("Or click STOP in Thonny to return to the REPL.")
+while button_left() == False:
+    sleep(0.25)
+
+
+#set varialbles and initial states
+current_state = 'forward' # Initial state of the robot
+object_detected = False # Flag to indicate if an object is detected
+state_updated = True # Flag to indicate if the state has been updated
+
+
+
+while True:
+    
+    ############################################
+    #                  See                     #
+    ############################################
+    start_ticks = tick_count
+    start_time = ticks_us()
+
+
+    distance_mm = SeeFunctions.filtered_distance()
+    if distance_mm < recognition_distance:  # If the distance is less than the recognition distance
+        object_detected = True
+
+    sensor_vals = SeeFunctions.read_IR_sensors()
+
+    delta_ticks = tick_count - start_ticks
+    delta_time = ticks_diff(ticks_us(), start_time) / 1_000_000  # seconds
+
+    # Compute wheel speed
+    motor_rps = delta_ticks / PPR / delta_time
+    wheel_rps = motor_rps / GEAR_RATIO
+    wheel_circ = math.pi * WHEEL_DIAMETER_CM
+    speed_cm_s = wheel_rps * wheel_circ
+
+    ############################################
+    #                 Think                    #
+    ############################################
+
+    error = ThinkFunctions.compute_error(sensor_vals, method='binary')
+
+"""
+    # Implement the line-following state machine transitions
+    if current_state == 'forward':
+        counter = 0
+        if line_right and not line_left:
+            current_state = 'turn_right'
+            state_updated = True
+        elif line_left and not line_right:
+            current_state = 'turn_left'
+            state_updated = True
+        elif line_left and line_right and line_center: # lost the line
+            current_state = 'turn_left'
+            state_updated = True
+        elif line_left and line_center and not line_right:
+            current_state = 'forward'
+            state_updated = True
+        elif button_right.value() == True:
+            current_state = 'stop'
+            state_updated = True
+            
+            
+    if current_state == 'turn_right':
+        if counter >= COUNTER_MAX:
+            current_state = 'forward'
+            state_updated = True
+        elif button_right.value() == True:
+            current_state = 'stop'
+            state_updated = True
+
+    if current_state == 'turn_left':
+        if counter >= COUNTER_MAX:
+            current_state = 'forward'
+            state_updated = True
+        elif button_right.value() == True:
+            current_state = 'stop'
+            state_updated = True
+            
+    if current_state == 'stop':
+        led_board.value(1)
+        if counter >= COUNTER_STOP:
+            current_state = 'forward'
+            state_update = True
+            led_board.value(0)
+"""            
+    
+    ############################################
+    #                  Act                     #
+    ############################################
+"""
+    # Send the new state when updated
+    if state_updated == True:
+        uart.write(current_state + '\n')
+        state_updated = False
+
+    counter += 1    # increment counter
+    sleep(0.02)     # wait 0.02 seconds
+   
+
+"""
