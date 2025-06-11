c2b5b390048cdae0356cb6baf7fca2346c601633
MerranRomp <merran@evabits.com>
2025-06-11 15:14:14 +0200

    added a compute_error function for the line following

--- Robot_Stuff/ESP32_cONTROLLER/Utils/ThinkFunctions.py
+++ Robot_Stuff/ESP32_cONTROLLER/Utils/ThinkFunctions.py
@@ -13,3 +13,10 @@ def line_position_binary(binary_vals):
 def line_position_inverse(inverse_vals):
     return sum(w * v for w, v in zip(weights, inverse_vals)) / max(sum(inverse_vals), 1)
 
+def compute_error(norm_or_bin_vals, method='inverse'):
+    if method == 'inverse':
+        return line_position_inverse(norm_or_bin_vals)
+    elif method == 'binary':
+        return line_position_binary(norm_or_bin_vals)
+    else:
+        raise ValueError("Unknown method. Use 'inverse' or 'binary'")
