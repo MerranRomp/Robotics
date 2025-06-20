d82c20735b4399bcafea504ffa568bd88634a811
Sven050423 <szuidema0504@gmail.com>
2025-06-20 11:58:54 +0200

    Updated text

--- Robot_Stuff/Wirless connection/Wirless_Graph.py
+++ Robot_Stuff/Wirless connection/Wirless_Graph.py
@@ -54,9 +54,10 @@ def serial_reader():
                     robot_node = line
                     print(f"Received node: {robot_node}")
                 else:
-                    print(f"Ignored unknown node: {line}")
+                    print(f"No node other data: {line}")
     except Exception as e:
         print("Serial error:", e)
+        
 
 # Start serial reader in the background
 threading.Thread(target=serial_reader, daemon=True).start()
